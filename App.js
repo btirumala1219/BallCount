1dbecf03ebbcdddbd0627c9d8526f43d7d1d7f53
btirumala <barath.tirumala@gmail.com>
2018-06-18 22:56:36 -0400

    Updated interface

--- App.js
+++ App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
 
-export default class Touchables extends Component {
+export default class App extends Component {
 
 
-  _onPressButton() {
-    Alert.alert("check")
+
+
+
+  constructor(props) {
+    super(props)
+    this.state = { ft: 0 , tw: 0, th: 0, fttot: 0, twtot: 0, thtot: 0 }
   }
 
+  _onPressButtonft = () => {
+    this.setState({
+      ft: this.state.ft+1,
+      fttot: this.state.fttot+1
+    })
+  }
+  _onPressButtontw = () => {
+    this.setState({
+      tw: this.state.tw+1,
+      twtot: this.state.twtot+1
+    })
+  }
+  _onPressButtonth = () => {
+    this.setState({
+      th: this.state.th+1,
+      thtot: this.state.thtot+1
+    })
+  }
 
-
+  _onPressButtonfttot = () => {
+    this.setState({
+      fttot: this.state.fttot+1
+    })
+  }
+  _onPressButtontwtot = () => {
+    this.setState({
+      twtot: this.state.twtot+1
+    })
+  }
+  _onPressButtonthtot = () => {
+    this.setState({
+      thtot: this.state.thtot+1
+    })
+  }
 
   render() {
     return (
@@ -20,41 +56,44 @@ export default class Touchables extends Component {
           </View>
 
         </View>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', top: '50%'}}>
-          <View style={{ flex: 1}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center'}}>
+          <View style={{ flex: 1,}}>
+            <Text style={styles.perctext}>{this.state.ft}/{this.state.fttot}</Text>
             <Text style={styles.shottext}>FT</Text>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtonft} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Made Shot</Text>
               </View>
             </TouchableHighlight>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtonfttot} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Miss Shot</Text>
               </View>
             </TouchableHighlight>
           </View>
-          <View style={{ flex: 1}}>
+          <View style={{ flex: 1,}}>
+            <Text style={styles.perctext}>{this.state.tw}/{this.state.twtot}</Text>
             <Text style={styles.shottext}>2 Pt.</Text>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtontw} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Made Shot</Text>
               </View>
             </TouchableHighlight>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtontwtot} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Miss Shot</Text>
               </View>
             </TouchableHighlight>
           </View>
-          <View style={{ flex: 1}}>
+          <View style={{ flex: 1,}}>
+            <Text style={styles.perctext}>{this.state.th}/{this.state.thtot}</Text>
             <Text style={styles.shottext}>3 Pt.</Text>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtonth} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Made Shot</Text>
               </View>
             </TouchableHighlight>
-            <TouchableHighlight style={styles.buttons} onPress={this._onPressButton} underlayColor="white">
+            <TouchableHighlight style={styles.buttons} onPress={this._onPressButtonthtot} underlayColor="white">
               <View style={styles.button}>
                 <Text style={styles.buttonText}>Miss Shot</Text>
               </View>
@@ -87,7 +126,7 @@ const styles = StyleSheet.create({
     width: '80%',
     alignSelf: 'center',
     alignItems: 'center',
-    top: '10%'
+    top: '45%'
   },
   buttonText: {
     padding: 20,
@@ -101,10 +140,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   shottext: {
-    top: 20,
+    top: "40%",
     color: 'orange',
     fontWeight: 'bold',
     fontSize: 30,
     alignSelf: 'center'
+  },
+  perctext: {
+    top: '30%',
+    color: 'red',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
   }
 })
